60100956423c6ce59981686ee95fcd47d9f23b5c
Certseeds <nanoseedskc@gmail.com>
2020-02-24 20:24:03 +0800

    update week2's exercise,update license and readme.

---
+++ week02/lab2_b.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+@Github: https://github.com/Certseeds
+@Organization: SUSTech
+@Author: nanoseeds
+@Date: 2020-02-24 16:58:42 
+@LastEditors  : nanoseeds
+@LastEditTime : 2020-02-24 16:58:42 
+'''
